960ca5f8764658eaf0fac2bfad6eb251c8ef6c1d
Paul Barrass <paul.barrass@gmail.com>
2016-09-13 11:52:39 +0100

    Point at correct branch for editor api

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -7,7 +7,7 @@ export default class HomeView extends React.Component {
 	render() {
 		return (
 			<div>
-				<iframe src='http://courses.dreamm.co.uk/roche/d1092/master/m1/index.html'
+				<iframe src='http://courses.dreamm.co.uk/roche/d1092/babel5to6/m1/index.html'
 						className={classes.iframe} frameBorder={0}/>
 				<div className={classes.editor}>
 					Editor
