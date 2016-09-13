47060a2a461900c9eea2257309d30fb16ef58081
Paul Barrass <paul.barrass@gmail.com>
2016-09-13 14:05:33 +0100

    Remember event.source to reply to messages

--- src/api/api.js
+++ src/api/api.js
@@ -1,23 +1,36 @@
 class ParentApiInstance {
-	handleMessage(event){
+	contentEventSource = null;
+
+	handleMessage(event) {
 		console.log('rec msg in editor!', event.data);
-		if(event.data.type){
-			switch(event.data.type){
+		if (event.data.type) {
+			switch (event.data.type) {
 				case 'init':
+					this.contentEventSource = event.source;
 					this.initContentEditor(event.data.params);
 					break;
 			}
 		}
 	}
 
-	initContentEditor({defaults, textItems}){
-		console.log('editor init', defaults);
+	sendMessageToContent(message) {
+		if (!this.contentEventSource) {
+			console.log(`error: sendMessageToContent() editor not initialised`, message);
+			return;
+		}
+
+		window.frames[0].postMessage({isEditorMessage: true, ...message}, '*');
 	}
 
-	init(){
+	initContentEditor({defaults, textItems}) {
+		console.log('editor init', defaults);
+		this.sendMessageToContent({type: 'editorFound'});
+	}
+
+	init() {
 		window.addEventListener('message', event => {
 			// crude but effective message filtering
-			if(event.data.isEditorMessage)
+			if (event.data.isEditorMessage)
 				this.handleMessage(event);
 		});
 	}
