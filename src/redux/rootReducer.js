b1c728572d719f6df1acb0c7234713c00770ab9c
Paul Barrass <paul.barrass@gmail.com>
2016-09-14 11:04:47 +0100

    Add textItems to root reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
-import currentUser from './modules/currentUser';
+import textItems from './modules/textItems';
 
 export default combineReducers({
-	currentUser,
+	textItems,
 	router
 });
