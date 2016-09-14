bef08c9182903a97ead48615174878ed64347840
Paul Barrass <paul.barrass@gmail.com>
2016-09-14 17:33:59 +0100

    Apply material design style to editor panels

--- src/views/ContentEditorView/ContentEditorView.js
+++ src/views/ContentEditorView/ContentEditorView.js
@@ -5,18 +5,41 @@ import classes from './contentEditor.css';
 import ParentEditorApi from '../../api/api';
 import SingleTextItem from 'components/singleTextItem/singleTextItem';
 
+import _ from 'lodash';
+
 class ContentEditorView extends React.Component {
 	static propTypes = {
 		textItems: PropTypes.object.isRequired
 	};
 
+	static keySuffixesToIgnore = ['transcripttext'];
+
 	static contextTypes = {
 		dispatch: PropTypes.any
 	};
 
+	constructor(props) {
+		super(props);
+		this.state = {height: window.innerHeight};
+	}
+
+	componentWillMount() {
+		this.resizeListener = _.debounce(this.windowResize.bind(this), 200);
+		window.addEventListener('resize', this.resizeListener);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.resizeListener);
+	}
+
 	componentDidMount() {
 		const {dispatch} = this.context;
 		ParentEditorApi.init(dispatch);
+	}
+
+	windowResize() {
+		const {innerHeight: height} = window;
+		this.setState({height});
 	}
 
 	textItemChanged({key, value}) {
@@ -25,18 +48,44 @@ class ContentEditorView extends React.Component {
 
 	render() {
 		const {textItems} = this.props;
+		const {height} = this.state;
 		const textItemKeys = Object.keys(textItems);
+		const groups = _.groupBy(_.filter(textItemKeys, (key) => {
+			// filter out ignored suffixes
+			const splitKey = key.split('-');
+			const suffix = splitKey[splitKey.length - 1];
+			return !_.includes(ContentEditorView.keySuffixesToIgnore, suffix);
+		}), (key) => {
+			// group by everything but the last 2 parts of the key
+			const splitKey = key.split('-');
+			return Array.join(_.take(splitKey, splitKey.length - 2), '-');
+		});
+
+		const groupKeys = Object.keys(groups);
+		const editorStyle = {height};
 
 		return (
 			<div>
 				<iframe id='contentframe' src='http://courses.dreamm.co.uk/roche/d1092/babel5to6/m1/index.html'
 						className={classes.iframe} frameBorder={0}/>
-				<div className={classes.editor}>
-					{textItemKeys.map(key => <SingleTextItem
-						key={key}
-						textItemKey={key}
-						textItemValue={textItems[key]}
-						onChange={this.textItemChanged} />)}
+				<div className={classes.editor} style={editorStyle}>
+					{groupKeys.map(groupKey => {
+						let splitKey = groupKey.split('-');
+						if (splitKey[splitKey.length - 1] === splitKey[splitKey.length - 2]) {
+							splitKey = _.take(splitKey, splitKey.length - 1);
+						}
+						const displayKey = splitKey.join(' → ');
+
+						return <div className={classes.group}>
+							<div className={classes.groupTitle}>{displayKey}</div>
+							{groups[groupKey].map(key => <SingleTextItem
+								key={key}
+								textItemKey={key}
+								textItemValue={textItems[key]}
+								onChange={this.textItemChanged}/>)}
+						</div>;
+					})}
+
 				</div>
 			</div>
 		);
