cd58645882cc3f0c6599ad0afd5ad56e6f337a88
Paul Barrass <paul.barrass@gmail.com>
2016-09-23 15:10:19 +0100

    Match editor for any route

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ import CoreLayout from 'layouts/CoreLayout/CoreLayout';
 import ContentEditorView from 'views/ContentEditorView/ContentEditorView';
 
 export default (store) => (
-  <Route path='/' component={CoreLayout}>
+  <Route path='/**' component={CoreLayout}>
     <IndexRoute component={ContentEditorView} />
   </Route>
 );
